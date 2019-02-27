54871e37d2a0e72d739a662417d1abc3688fc24d
Khem Sok <khemsok97@gmail.com>
2019-02-27 14:32:55 -0500

    add pic feature

--- cnn_server.py
+++ cnn_server.py
@@ -46,10 +46,10 @@ def handle_data():
         print('THE RESULT IS ' + prediction)
 
     return render_template('index.html', prediction=prediction.upper())
-    
-if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
 
-# app.run()
+# if __name__ == '__main__':
+#      app.debug = True
+#      port = int(os.environ.get("PORT", 5000))
+#      app.run(host='0.0.0.0', port=port)
+
+app.run()
